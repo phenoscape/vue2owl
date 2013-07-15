40234a0969770dc2d0681d895df7ba3cab346dfa
Jim Balhoff <balhoff@nescent.org>
2013-07-15 16:23:32 -0500

    Initial commit.

---
+++ src/main/scala/org/nescent/vue/VUE2OWL.scala
@@ -0,0 +1,71 @@
+package org.nescent.vue
+
+import scala.io.Source
+import scala.xml.XML
+import scala.collection.mutable
+import scala.collection.JavaConversions._
+import java.io.InputStreamReader
+import java.io.FileInputStream
+import scala.xml.Node
+import org.semanticweb.owlapi.model.OWLEntity
+import org.semanticweb.owlapi.model.IRI
+import org.semanticweb.owlapi.apibinding.OWLManager
+import java.io.File
+import org.semanticweb.owlapi.model.OWLNamedIndividual
+import org.semanticweb.owlapi.model.OWLClass
+import scala.None
+import org.semanticweb.owlapi.model.OWLObjectProperty
+import org.semanticweb.owlapi.model.OWLObjectPropertyExpression
+
+object VUE2OWL extends App {
+
+	val factory = OWLManager.getOWLDataFactory();
+	val manager = OWLManager.createOWLOntologyManager();
+	val ontology = manager.createOntology();
+	val cleanedInput = Source.fromFile(args(0), "US-ASCII").getLines.dropWhile(_.startsWith("<!--")).mkString;
+	val input = XML.loadString(cleanedInput);
+	def isNode(elem: Node): Boolean = {
+			(elem \ "@{http://www.w3.org/2001/XMLSchema-instance}type").head.text == "node";
+	}
+	def isLink(elem: Node): Boolean = {
+			(elem \ "@{http://www.w3.org/2001/XMLSchema-instance}type").head.text == "link";
+	}
+	val terms: mutable.Map[String, OWLEntity] = mutable.Map();
+	(input \ "child").filter(isNode(_)).foreach(elem => {
+		val id = (elem \ "@ID").head.text;
+		val iri = IRI.create((elem \ "URIString").head.text);
+		(elem \ "@label").foreach(attrValue => manager.addAxiom(ontology, factory.getOWLAnnotationAssertionAxiom(factory.getRDFSLabel(), iri, factory.getOWLLiteral(attrValue.text))));
+		val termOption = (elem \ "shape" \ "@{http://www.w3.org/2001/XMLSchema-instance}type").head.text match {
+		case "roundRect" => Some(factory.getOWLClass(iri));
+		case "ellipse" => Some(factory.getOWLNamedIndividual(iri));
+		case _ => None;
+		}
+		termOption.foreach(term => {
+			terms.put(id, term);
+			manager.addAxiom(ontology, factory.getOWLDeclarationAxiom(term));
+		});
+	});
+	(input \ "child").filter(isLink(_)).foreach(elem => {
+		val sourceID = (elem \ "ID1").head.text;
+		val targetID = (elem \ "ID2").head.text;
+		val source = terms(sourceID);
+		val target = terms(targetID);
+		val relation = (elem \ "@label").headOption.map(attr => factory.getOWLObjectProperty(IRI.create(attr.text)));
+		println(relation);
+		(source, target, relation) match {
+		case (individual: OWLNamedIndividual, ontClass: OWLClass, None) => {
+			manager.addAxiom(ontology, factory.getOWLClassAssertionAxiom(ontClass, individual));
+		}
+		case (subclass: OWLClass, superclass: OWLClass, None) => {
+			manager.addAxiom(ontology, factory.getOWLSubClassOfAxiom(subclass, superclass));
+		}
+		case (subclass: OWLClass, superclass: OWLClass, Some(property)) => {
+			manager.addAxiom(ontology, factory.getOWLSubClassOfAxiom(subclass, factory.getOWLObjectSomeValuesFrom(property, superclass)));
+		}
+		case _ => {}
+		}
+	});
+	manager.saveOntology(ontology, IRI.create(new File(args(1))));
+
+
+}
